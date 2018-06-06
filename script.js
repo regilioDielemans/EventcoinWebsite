9c0e14bd3ba5cc38b7b9ddf573e61b8a5ab6d17e
Rowan Dorst <rdorst1@avans.nl>
2018-06-06 14:57:04 +0200

    commit van laptop zodat ik op pc verder kan

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 function UserLogin() {
-var params = 'name'
+	var params = "userId=5&firstname=voornaam&insertion=van de&lastname=achternaam&dateOfBirth=1999-05-23&email=naam@avans.nl&role=1";
+	var url = "https://eventcoin.herokuapp.com/api/login";
 
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "https://eventcoin.herokuapp.com/api/login", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
 
-    xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+   
+    var data = {"email": "test@avans.nl", "password": "test"};
+    xhttp.send(data);
+    xhttp.onreadystatechange = function(){
+    	if (xhttp.readyState === 4 && xhttp.status === 200) {
+    		var json = JSON.parse(xhttp.responseText);
+    		console.log(json.email + ", " + json.password);
+    	}
+    }
+    //console.log(data)
+   	//var response = xhttp.responseText;
+   	//console.log(response);
+   		//console.log(response);
+    //document.getElementById(responsP).innerHTML = response;
 }
